2d80c411a3a156d91d2046bc8fd165412da37fa7
MarianKurtov <mariankurtov@gmail.com>
2020-12-29 15:29:25 +0200

    Contacts are ready

--- dim/Models/ContactFormViewModel.cs
+++ dim/Models/ContactFormViewModel.cs
@@ -7,21 +7,21 @@ namespace dim.Models
     public class ContactFormViewModel
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашите имена")]
-        [Display(Name = "Вашите имена")]
+        [Display(Name = "Вашите имена:")]
         public string Name { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашият email адрес")]
         [EmailAddress(ErrorMessage = "Моля въведете валиден email адрес")]
-        [Display(Name = "Вашите имейл адрес")]
+        [Display(Name = "Вашия имейл адрес:")]
         public string Email { get; set; }
 
         [Required]
         [MinLength(10)]
-        [Display(Name = "Вашия телефон")]
+        [Display(Name = "Вашия телефон:")]
         public string Phone { get; set; }
 
         [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете съдържание на съобщението")]
-        [Display(Name = "Съдържание на съобщението")]
+        [Display(Name = "Съдържание на съобщението:")]
         public string Content { get; set; }
     }
 }
