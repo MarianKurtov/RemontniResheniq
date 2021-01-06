488b166b5cff81740a8e242d596cc8ccea5401bc
MarianKurtov <mariankurtov@gmail.com>
2021-01-06 16:29:40 +0200

    Finish now!

--- dim/Models/ContactFormViewModel.cs
+++ dim/Models/ContactFormViewModel.cs
@@ -1,26 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace dim.Models
 {
     public class ContactFormViewModel
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашите имена")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашите имена!")]
         [Display(Name = "Вашите имена:")]
         public string Name { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашият email адрес")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашия email адрес!")]
         [EmailAddress(ErrorMessage = "Моля въведете валиден email адрес")]
         [Display(Name = "Вашия имейл адрес:")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете вашия телефонен номер!")]
         [MinLength(10)]
-        [Display(Name = "Вашия телефон:")]
+        [Display(Name = "Вашия телефонен номер:")]
         public string Phone { get; set; }
 
-        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете съдържание на съобщението")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Моля въведете съдържание на съобщението!")]
         [Display(Name = "Съдържание на съобщението:")]
         public string Content { get; set; }
     }
